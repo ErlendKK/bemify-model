74bf92eaf03bbe3e75475bf582328ba6a98a2b0e
ErlendKK <136587757+ErlendKK@users.noreply.github.com>
2025-03-15 23:40:25 +0100

    Updated model

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import flask
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return flask.jsonify({
+        "message": "Hello World!",
+        "status": "success"
+    })
 
 if __name__ == '__main__':
     app.run()
